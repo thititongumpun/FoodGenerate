a08f8be4ca19aac51978a908eec4d57f6389733c
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-02-21 20:29:42 +0700

    init vuex

--- foodgenerate-vue/src/App.vue
+++ foodgenerate-vue/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <FoodGenerate msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FoodGenerate from './components/FoodGenerate.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FoodGenerate,
   }
 }
 </script>
---
+++ foodgenerate-vue/src/store/index.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+const state = {
+  posts: []
+}
+
+const getters = {}
+
+const actions = {
+  getPosts({ commit }) {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+      commit('SET_POSTS', res.data)
+    })
+  }
+}
+
+const mutations = {
+  SET_POSTS(state, posts) {
+    state.posts = posts
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations
+})
